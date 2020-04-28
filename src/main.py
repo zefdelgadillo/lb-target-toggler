5ff3b509d3ce56e0b5e4a61df04cfdd1803d0320
Zef Delgadillo <zef.delgadillo@gmail.com>
2020-04-28 10:26:47 -0400

    use google python style

--- src/main.py
+++ src/main.py
@@ -8,20 +8,23 @@ PROJECT = os.environ.get('GCP_PROJECT')
 
 compute = build('compute', 'v1')
 
+
 def main(event, context):
-  print('Event: ' + str(event))
-  print('Context: ' + str(context))
-  update_url_map()
+    print('Event: ' + str(event))
+    print('Context: ' + str(context))
+    update_url_map()
+
 
 def update_url_map():
-  target_service = f'https://www.googleapis.com/compute/v1/projects/{PROJECT}/global/backendBuckets/{BACKEND_BUCKET}'
-  body = {
-    'defaultService': target_service
-  }
-  try:
-    request = compute.urlMaps().patch(project=PROJECT, urlMap=LOAD_BALANCER, body=body, requestId=uuid.uuid4())
-    response = request.execute()
-    print(response)
-    return response
-  except Exception as e:
-    print(e)
+    target_service = f'https://www.googleapis.com/compute/v1/projects/{PROJECT}/global/backendBuckets/{BACKEND_BUCKET}'
+    body = {'defaultService': target_service}
+    try:
+        request = compute.urlMaps().patch(project=PROJECT,
+                                          urlMap=LOAD_BALANCER,
+                                          body=body,
+                                          requestId=uuid.uuid4())
+        response = request.execute()
+        print(response)
+        return response
+    except Exception as e:
+        print(e)
